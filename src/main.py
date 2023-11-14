4e8e3cdbcd017b411fa331669ec10ec1fe405029
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-11-13 22:59:44 -0300

    feat: websocket na main

--- src/main.py
+++ src/main.py
@@ -1,23 +1,17 @@
-from colorama import (
-    init as init_colorama,
-    Fore
-)
+import asyncio
+from colorama import (init as init_colorama, Fore)
 from os import path
-
 from graph import LinearGraph
+from socket_handler import SocketHandler
+import json
+import nest_asyncio
 
-
-OUTPUT_FOLDER = path.abspath(
-    path.join(
-        path.dirname(__file__),
-        '..', 'output'
-    )
-)
+nest_asyncio.apply()
+OUTPUT_FOLDER = path.abspath(path.join(path.dirname(__file__),'..', 'output'))
 GRAPH_FILE = path.join(OUTPUT_FOLDER, 'graph.json')
 SOLUTION_FILE = path.join(OUTPUT_FOLDER, 'solution.json')
 
-
-if __name__ == '__main__':
+async def main():
     init_colorama(autoreset=True)
     print(
         '| ----------------------------|\n'
@@ -25,16 +19,25 @@ if __name__ == '__main__':
         '| davidsantana06 -- alissonfr |\n'
         '| --------------------------- |'
     )
+    linear_graph = LinearGraph()
 
-    graph = LinearGraph()
-
-    input('\nPressione qualquer tecla para gerar o grafo...')
-    graph.generate_graph(
-        paths=5, min_edges=500, max_edges=1000, min_distance=1, max_distance=10, 
-        output_file=GRAPH_FILE
-    )
+    linear_graph.generate(paths=6, min_edges=500, max_edges=1000, min_distance=1, max_distance=10, output_file=GRAPH_FILE)
     print(f'O grafo foi gerado em {Fore.CYAN}"{GRAPH_FILE}"')
 
+    socket_handler = SocketHandler()
+    socket_handler.start_server()
+
     input('\nPressione qualquer tecla para resolver o grafo...')
-    graph.solve_graph(GRAPH_FILE, SOLUTION_FILE)
-    print(f'A solução foi gerada em {Fore.CYAN}"{SOLUTION_FILE}"')
+    graph = linear_graph.get_graph(GRAPH_FILE)
+
+    try:
+        for path in enumerate(graph['paths']):
+            await socket_handler.send_message(json.dumps(path))
+            await asyncio.sleep(1)
+        linear_graph.get_solution(socket_handler.get_path_solvers(), SOLUTION_FILE)
+        print(f'A solução foi gerada em {Fore.CYAN}"{SOLUTION_FILE}"')
+    finally:
+        socket_handler.stop_server()
+
+if __name__ == '__main__':
+    asyncio.run(main())
