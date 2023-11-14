604931f5bd8059fdb2518518a240d8f1d201253a
davidsantana06 <davidsantana06@hotmail.com>
2023-11-14 12:34:22 -0300

    refactor: optmize code structure

--- src/socket_handler.py
+++ src/socket_handler.py
@@ -1,43 +1,49 @@
-import asyncio
-import websockets
+from colorama import Fore
+from os import environ
 from typing import List
+from websockets import WebSocketServerProtocol
+import asyncio
 import json
-from graph.path_solver import PathSolver
+import websockets
+
+from graph import PathSolver
+
 
 class SocketHandler:
-    def __init__(self, host='localhost', port=8080):
-        self.host = host
-        self.port = port
-        self.server = None
-        self.clients = set()
-        self.path_solvers: List[PathSolver] = []
+    def __init__(self):
+        self.__host = environ.get('HOST')
+        self.__port = int(environ.get('PORT'))
+        self.__server = None
+        self.__clients: List[WebSocketServerProtocol] = []
+        self.__path_solvers: List[PathSolver] = []
 
-    async def handle_client(self, websocket, path):
-        self.clients.add(websocket)
+    @property
+    def path_solvers(self):
+        return self.__path_solvers
+
+    async def handle_client(self, websocket: WebSocketServerProtocol):
+        self.__clients.append(websocket)
+
         try:
             async for message in websocket:
                 data = json.loads(message)
-                print(f"O caminho {data['id']} foi resolvido pelo cliente com sucesso!")
-                self.path_solvers.append(data)
+                self.__path_solvers.append(data)
+                print(f'O caminho {data["id"]} foi resolvido pelo cliente com sucesso!')
         finally:
-            self.clients.remove(websocket)
+            self.__clients.remove(websocket)
 
-    async def send_message(self, message):
-        if self.clients:
-            print("Enviando caminho para o cliente...")
-            await asyncio.gather(*[client.send(message) for client in self.clients])
+    async def send_message(self, message: str):
+        if self.__clients:
+            print('\nEnviando caminho para o cliente...')
+            await asyncio.gather(*[client.send(message) for client in self.__clients])
 
     def start_server(self):
-        start_server = websockets.serve(self.handle_client, self.host, self.port)
-
-        self.server = asyncio.get_event_loop().run_until_complete(start_server)
-        print(f"🚀  Servidor WebSocket iniciou em ws://{self.host}:{self.port}...")
-
-    def get_path_solvers(self):
-        return self.path_solvers
+        start_server = websockets.serve(self.handle_client, self.__host, self.__port)
+        self.__server = asyncio.get_event_loop().run_until_complete(start_server)
+        print(f'O servidor WebSocket iniciou em {Fore.GREEN}ws://{self.__host}:{self.__port}')
 
     def stop_server(self):
-        if self.server:
-            self.server.close()
-            asyncio.get_event_loop().run_until_complete(self.server.wait_closed())
-            print("Servidor WebSocket foi interrompido.")
+        if self.__server:
+            self.__server.close()
+            asyncio.get_event_loop().run_until_complete(self.__server.wait_closed())
+            print(f'\nO servidor WebSocket foi interrompido')
